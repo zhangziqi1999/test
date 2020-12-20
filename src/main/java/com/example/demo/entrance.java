67d1c5dc07c434cdac51d0fbc68a2937a2176321
solo <1327698867@qq.com>
2020-12-20 09:15:12 +0800

    i

--- src/main/java/com/example/demo/entrance.java
+++ src/main/java/com/example/demo/entrance.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class entrance {
     @PostMapping("/upload")
-    String getMessage(String json){
+    String getMessage(@RequestBody String json){
         System.out.println(json);
         return "ok";
     }
