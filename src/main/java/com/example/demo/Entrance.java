2e55ea4186d0b7ee56018c57fd99e59af9434468
solo <1327698867@qq.com>
2020-12-22 20:15:03 +0800

    1

--- src/main/java/com/example/demo/Entrance.java
+++ src/main/java/com/example/demo/Entrance.java
@@ -3,10 +3,7 @@ package com.example.demo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Entrance {
@@ -26,6 +23,7 @@ public class Entrance {
     }
 
     @GetMapping("/download")
+    @CrossOrigin(origins = "*", maxAge = 3600)
     DataEntity[] getMessage(){
         return dao.getData();
     }
